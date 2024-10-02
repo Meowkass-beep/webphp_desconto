40dd316267daa41d5d988a8251119f77ae8ee3df
M3owk4s <milky.underr@gmail.com>
2024-10-02 14:45:37 -0300

    v2

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,14 +8,30 @@
 
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <h1>Calcular preço final com desconto</h1>
-    <form action="desconto.php" method="get">
-        <label for="preco">Preço:</label>
-        <input name="preco" type="number" placeholder="Digite o preço" required>
-        <br>
-        <br>
-        <input type="submit">
-    </form>
+    <div id="container">
+        <h1>Calcular preço final com desconto</h1>
+        <form action="index.php" method="get">
+            <label for="preco">Preço:</label>
+            <input name="preco" type="number" placeholder="Digite o preço" required>
+            <br>
+            <br>
+            <input type="submit">
+        </form>
+
+        <?php
+        if (isset($_GET["preco"])) {
+            $preco = $_GET["preco"];
+            $desconto = 10;
+            echo "Preço inicial: R$$preco <br>";
+            echo "Desconto: $desconto% <br>";
+
+            $precoFinal = $preco - ($preco * ($desconto / 100));
+            echo "Preço final: R$$precoFinal";
+        }
+        ?>
+    </div>
 </body>
+
 </html>
